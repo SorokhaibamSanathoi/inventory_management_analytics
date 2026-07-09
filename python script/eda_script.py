9058b1501eee31bb51d08f347717b4cc464ebe8f
Sanathoi Sorokhaibam <sanameitei1234560987@gmail.com>
2026-07-10 01:02:12 +0530

    Add 2 more eda script

--- python script/eda_script.py
+++ python script/eda_script.py
@@ -18,7 +18,7 @@ import pandas as pd
 
 Daily_usage = pd.read_csv("data/cleaned_data/daily_usage.csv")
 Items = pd.read_csv("data/cleaned_data/items.csv")
-
+Restock = pd.read_csv("data/cleaned_data/restock.csv")
 
 # =====================================================
 # Business Question 1:
@@ -70,6 +70,27 @@ Items = pd.read_csv("data/cleaned_data/items.csv")
 
 # =====================================================
 # Business Question 4:
-# Which number of restock per client and name of the item that cause the restock 
+# Which site requires the highest number of emergency restocks?
 # =====================================================
 
+# total_emergency_restock = Restock[Restock["restock_type"] == "emergency"].groupby("site_id")["restock_type"].count().reset_index()
+# total_emergency_restock.columns = ["site_id","emergency_restock"]
+# print(total_emergency_restock.sort_values("emergency_restock",ascending=False))
+
+
+# =====================================================
+# Business Question 5:
+# which items triggered these emergency restocks?
+# =====================================================
+
+emergency_restock = (
+    Restock[Restock["restock_type"] == "emergency"]
+    .groupby("item_id")["restock_type"]
+    .count().
+    reset_index()
+)
+emergency_restock = emergency_restock.merge(
+    Items[["item_id","item_name"]],
+    on = "item_id"
+)
+print(emergency_restock)
