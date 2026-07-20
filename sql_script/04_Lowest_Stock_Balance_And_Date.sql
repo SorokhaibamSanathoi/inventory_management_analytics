3ce88d4a091ef4c4ce412f6ff4e7e6a94e1b3934
Sanathoi Sorokhaibam <sanameitei1234560987@gmail.com>
2026-07-20 21:50:51 +0530

    Added query to find out avg daily consumption for each item

--- sql_script/04_Lowest_Stock_Balance_And_Date.sql
+++ sql_script/04_Lowest_Stock_Balance_And_Date.sql
@@ -1,61 +1,81 @@
--- combining the restock balance and consumption data
-
---positive values
+-- Combine restock (+) and consumption (-) into one table
 WITH unified_data AS (
+
+    -- Positive stock movement (Restock)
     SELECT
         r.item_id,
         r.site_id,
-        date AS date,
-        r.qty as quantity
+        date,
+        r.qty AS quantity
     FROM inventory_management_analytics.restock r
 
-UNION ALL
+    UNION ALL
 
---negative values
-    SELECT 
+    -- Negative stock movement (Consumption)
+    SELECT
         c.item_id,
         c.site_id,
-        date AS date,
-        -c.qty_drawn as quantity
+        date,
+        -c.qty_drawn AS quantity
     FROM inventory_management_analytics.daily_usage c
-    WHERE is_outlier ='False'
-    
+    WHERE is_outlier = 'False'
+
 ),
+
+-- Calculate running inventory balance
 Daily_balance AS (
-    SELECT 
+
+    SELECT
         u.item_id,
         u.site_id,
         u.date,
         COALESCE(o.opening_stock,0) +
-    SUM(u.quantity) OVER (
-        PARTITION BY u.item_id, u.site_id
-        ORDER BY u.date
-        ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
-) AS balance_stock
+        SUM(u.quantity) OVER (
+            PARTITION BY u.item_id, u.site_id
+            ORDER BY u.date
+            ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+        ) AS balance_stock
+
     FROM unified_data u
-    left join inventory_management_analytics.opening_stock o
-    on o.site_id = u.site_id and o.item_id = u.item_id
+    LEFT JOIN inventory_management_analytics.opening_stock o
+        ON o.site_id = u.site_id
+       AND o.item_id = u.item_id
+
 ),
+
+-- Rank balances from lowest to highest
 Ranked_Stocks AS (
-    SELECT 
+
+    SELECT
         site_id,
         item_id,
         date,
         balance_stock,
+
         RANK() OVER (
-            PARTITION BY site_id, item_id 
+            PARTITION BY site_id, item_id
             ORDER BY balance_stock ASC
-        ) as stock_rank
+        ) AS stock_rank
+
     FROM Daily_balance
+
 )
-SELECT 
+
+-- Show the lowest inventory balance for each item
+SELECT
     rs.site_id,
     rs.item_id,
     i.item_name,
     rs.date AS lowest_stock_date,
     rs.balance_stock AS lowest_stock_balance
+
 FROM Ranked_Stocks rs
+
 JOIN inventory_management_analytics.items i
     ON rs.item_id = i.item_id
+
 WHERE stock_rank = 1
-ORDER BY rs.site_id, lowest_stock_balance ASC;
+
+ORDER BY
+    rs.site_id,
+    lowest_stock_balance ASC;
