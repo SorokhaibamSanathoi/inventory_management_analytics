256b157d765c1cc72635b528505b35536b377543
Sanathoi Sorokhaibam <sanameitei1234560987@gmail.com>
2026-07-27 19:09:56 +0530

    Canges 1 sql script & added 2 View tables

---
+++ View_tables_for_Power_BI/02_Daily_Consumption_Summary.sql
@@ -0,0 +1,23 @@
+CREATE VIEW inventory_management_analytics.daily_consumption_summary AS
+    SELECT
+        d.date AS date,
+        d.site_id,
+        d.client_id,
+        d.item_id,
+        SUM(d.qty_drawn) AS  total_qty_drawn,
+
+        i.item_name,
+        i.category,
+        i.unit
+
+FROM inventory_management_analytics.daily_usage d
+INNER JOIN inventory_management_analytics.items i
+ON i.item_id = d.item_id
+GROUP BY 
+    d.date,
+    d.site_id,
+    d.item_id,
+    d.client_id,
+    i.item_name,
+    i.category,
+    i.unit
